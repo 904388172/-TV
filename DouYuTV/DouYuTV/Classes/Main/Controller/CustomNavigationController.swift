4bb44578d2a324e51f536f1e5ddf14be1db8578e
GS <gs@192.168.9.13>
2017-10-30 13:48:42 +0800

    添加全屏手势

--- DouYuTV/DouYuTV/Classes/Main/Controller/CustomNavigationController.swift
+++ DouYuTV/DouYuTV/Classes/Main/Controller/CustomNavigationController.swift
@@ -17,12 +17,44 @@ class CustomNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /**
+             添加全屏手势，支持滑动返回
+         */
+        //1.获取系统的Pop手势
+        guard let systemGes = interactivePopGestureRecognizer else { return }
+        //2.获取手势添加到View中
+        guard let gesView = systemGes.view else { return }
+        
+        //3.获取target／action
+        //3.1利用运行时机制查看所有的属性名称
+        /*
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+        for i in 0..<count {
+            let ivar = ivars[Int(i)]
+            let name = ivar_getName(ivar)
+            print(String(cString: name!))
+        }
+         */
+        let targets = systemGes.value(forKey: "_targets") as? [NSObject]
+        guard let targetObjc = targets?.first else { return }
+//        print(targetObjc)
+        
+        //3.2取出target
+        guard let target = targetObjc.value(forKey: "target") else { return }
+        
+        //3.3取出action
+        let action = Selector(("handleNavigationTransition:"))
+        
+        //3.4创建pan手势
+        let panGes = UIPanGestureRecognizer()
+        gesView.addGestureRecognizer(panGes)
+        panGes.addTarget(target, action: action)
+                
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     //重写push的方法
--- DouYuTV/DouYuTV/Classes/Room/Controller/RoomNormalViewController.swift
+++ DouYuTV/DouYuTV/Classes/Room/Controller/RoomNormalViewController.swift
@@ -21,9 +21,13 @@ class RoomNormalViewController: UIViewController, UIGestureRecognizerDelegate {
         //隐藏导航栏
         navigationController?.setNavigationBarHidden(true, animated: true)
         
-        //依然保存手势
+        /*
+         已经存在自定义全屏手势了就不需要系统的手势了
+         
+        //依然保存手势（可以滑动返回）
         navigationController?.interactivePopGestureRecognizer?.delegate = self
         navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+         */
     }
     
     override func viewDidLoad() {
